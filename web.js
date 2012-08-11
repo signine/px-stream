b77ce862d6887f9b786d554b0035a1ad415d5075
Shamil <shamil@ubuntu.ubuntu-domain>
2012-08-11 16:02:36 -0400

    without worker

--- web.js
+++ web.js
@@ -16,8 +16,10 @@ app.listen(port, function() {
 
 var app = require('express').createServer()
 var io = require('socket.io').listen(app)
+var request = require('request')
 
-var data = []
+
+var photos = []
 
 io.configure(function () { 
   io.set("transports", ["xhr-polling"]); 
@@ -27,22 +29,35 @@ io.configure(function () {
 app.listen( process.env.PORT || 3000)
 
 app.get('/', function (req, res) {
-	res.sendfile(__dirname + '/index.html')
+	res.render('index.ejs',{ layout:false, 
+							 locals: {  host: process.env.PORT ? 'http://px-stream.herokuapp.com' : 'http://localhost:3000', 
+							 		    data: JSON.stringify(photos)  }})
 });
 
-app.get('/fresh_today', function (req, res) {
-	res.sendfile(__dirname + '/fresh_today.html')
-});
 
-app.get('/input', function (req, res) {
-	if (data.length > 20)
-		data.pop()
-	data.unshift({url: req.query['url'], id: req.query['id']})
-	io.sockets.emit('fresh_today', {url: req.query['url'], id: req.query['id']})
-	res.end()	
-})
+setInterval(fetch_data, 1000)
 
-app.get('/populate', function (req, res) {
-	res.writeHead(200, {'Content-Type': 'text/plain'})
-	res.end(JSON.stringify(data))
-})
+var URL = BASE_URL = "https://api.500px.com/v1/photos?tags=1&feature=fresh_today&consumer_key=iGjydYfQtArfIFm9exTam4X07eE26GfPaDTgAqi6"
+
+var ARRAY_SIZE = 20
+var last_id = 0
+
+function fetch_data() {
+	request({
+		method: 'GET',
+		url: URL
+	}, function (err, res, body) {
+			var data = JSON.parse(body)['photos'].reverse()
+			for (var i in data) {
+				if (data[i]['id'] > last_id) {
+					last_id = data[i]['id']
+					if (photos.length > ARRAY_SIZE)
+						photos.pop()
+					var new_photo = { id: last_id,
+									 url: data[i]['images'][0]['url'] }
+					photos.unshift(new_photo)
+					io.sockets.emit('fresh_today', new_photo)
+				}
+			}
+	})
+}
