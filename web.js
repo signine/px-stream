c78ddf1402969643a5242dd6287975564d5a069d
Shamil <shamil@500px.com>
2012-09-08 00:10:44 -0400

    fix missing photos bug. Clean up

--- web.js
+++ web.js
@@ -1,18 +1,3 @@
-/*
-var express = require('express');
-
-var app = express.createServer(express.logger());
-
-app.get('/', function(request, response) {
-  response.send('Hello World!');
-});
-
-var port = process.env.PORT || 5000;
-app.listen(port, function() {
-  console.log("Listening on " + port);
-})	
-*/
-
 
 var app = require('express').createServer()
 var io = require('socket.io').listen(app)
@@ -41,19 +26,22 @@ var URL = BASE_URL = "https://api.500px.com/v1/photos?tags=1&feature=fresh_today
 
 var ARRAY_SIZE = 20
 var last_id = 0
+var photo_ids = []
 
 function fetch_data() {
 	request({
 		method: 'GET',
 		url: URL
 	}, function (err, res, body) {
-			var data = JSON.parse(body)['photos'].reverse()
+			var data = JSON.parse(body)['photos']
 			for (var i in data) {
-				if (data[i]['id'] > last_id && data[i]['nsfw'] == false) {
-					last_id = data[i]['id']
+				if (photo_ids.indexOf(data[i]['id']) <= -1 && data[i]['nsfw'] == false) {
+					if (photo_ids.length > 50)
+						photo_ids.pop();
+					photo_ids.unshift(data[i]['id'])
 					if (photos.length > ARRAY_SIZE)
 						photos.pop()
-					var new_photo = { id: last_id,
+					var new_photo = { id: data[i]['id'],
 									 url: data[i]['images'][0]['url'] }
 					photos.unshift(new_photo)
 					io.sockets.emit('fresh_today', new_photo)
